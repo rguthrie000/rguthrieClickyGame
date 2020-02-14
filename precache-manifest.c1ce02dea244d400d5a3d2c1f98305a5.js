12ad4ce0e95814c4af20cdd17d1ec00b5a6b182c
rguthrie <rguthrie000@gmail.com>
2020-02-14 13:20:10 -0500

    Updates

---
+++ precache-manifest.c1ce02dea244d400d5a3d2c1f98305a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b83c2bd37bc1f8563202a1187b67d22",
+    "url": "/rguthrieClickyGame/index.html"
+  },
+  {
+    "revision": "ceee6e48af88a14eb160",
+    "url": "/rguthrieClickyGame/static/css/main.fa192bb7.chunk.css"
+  },
+  {
+    "revision": "15bcde882cd0b919d2bb",
+    "url": "/rguthrieClickyGame/static/js/2.b0dd3cd5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/rguthrieClickyGame/static/js/2.b0dd3cd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceee6e48af88a14eb160",
+    "url": "/rguthrieClickyGame/static/js/main.52150aa0.chunk.js"
+  },
+  {
+    "revision": "c180b215ff83f1abc57b",
+    "url": "/rguthrieClickyGame/static/js/runtime-main.35e70818.js"
+  },
+  {
+    "revision": "7d09a485cb23ebed6cb999e0f4302dd6",
+    "url": "/rguthrieClickyGame/static/media/jsx.7d09a485.gif"
+  }
+]);
